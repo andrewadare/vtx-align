1fa09e321e34d51b4d9f6e162c7a96c89e350ae2
Andrew Adare <andrewadare@gmail.com>
2014-06-24 14:40:03 -0600

    Provide option to load geom lib or build from source

--- rootlogon.C
+++ rootlogon.C
@@ -11,40 +11,42 @@ void rootlogon()
          gSystem->Getenv("PWD"),
          gSystem->HostName());
 
-  if (gSystem->Getenv("TMPDIR"))
-    gSystem->SetBuildDir(gSystem->Getenv("TMPDIR"));
 
-  // Include paths
-  // =============
-  // There are 2 separate paths: one for ACLiC, and one for CINT or CLING.
-  // 1. ACLiC include path
-  const char *pwd = gSystem->WorkingDirectory();
-  const char *inc = pwd; //gSystem->DirName(pwd); // Parent directory
-  const char *geo = "/Users/adare/phenix/svxgeo";
-  const char *mpd = "/Users/adare/millepede";
+  bool atrcf = false;
 
-  gSystem->AddIncludePath(Form("-I%s ", inc));
-  gSystem->AddIncludePath(Form("-I%s ", geo));
-  gSystem->AddIncludePath(Form("-I%s ", mpd));
+  if (atrcf)
+    gSystem->Load("libsvxgeo"); // src: offline/packages/svxgeo
+  else
+  {
+    // For dev work / remote use: build shared libs with ACLiC.
+    // Set this to your source path
+    const char *geo = "/Users/adare/phenix/svxgeo";
 
-  // 2. Interpreter include path
-  // Type .include (ROOT 5) or .I (ROOT 6) at the ROOT REPL to see a listing
+    // There are 2 include paths: one for ACLiC, and one for CINT or CLING.
+    // Type .include (ROOT 5) or .I (ROOT 6) at the ROOT REPL to list.
+    gSystem->AddIncludePath(Form("-I%s ", geo));
 #ifdef __CINT__
-  gROOT->ProcessLine(Form(".include %s", inc));
-  gROOT->ProcessLine(Form(".include %s", geo));
+    gROOT->ProcessLine(Form(".include %s", geo));
+#endif
+#ifdef __CLING__
+    gROOT->ProcessLine(Form(".I %s", geo));
+#endif
+    gROOT->LoadMacro(Form("%s/SvxTGeo.C+", geo));
+    gROOT->LoadMacro(Form("%s/SvxGeoTrack.C+", geo));
+    gROOT->LoadMacro(Form("%s/SvxProj.C+", geo));
+  }
+
+  // Build library from Mille source
+  const char *mpd = "mille";
+  gSystem->AddIncludePath(Form("-I%s ", mpd));
+#ifdef __CINT__
   gROOT->ProcessLine(Form(".include %s", mpd));
 #endif
 #ifdef __CLING__
-  gROOT->ProcessLine(Form(".I %s", inc));
-  gROOT->ProcessLine(Form(".I %s", geo));
   gROOT->ProcessLine(Form(".I %s", mpd));
 #endif
 
-  // Build shared libs using ACLiC
   gROOT->LoadMacro(Form("%s/Mille.cc+", mpd));
-  gROOT->LoadMacro(Form("%s/SvxTGeo.C+", geo));
-  gROOT->LoadMacro(Form("%s/SvxGeoTrack.C+", geo));
-  gROOT->LoadMacro(Form("%s/SvxProj.C+", geo));
 
   return;
 }
