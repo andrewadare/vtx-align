7d0981ad56cf8e7467d0ca67b5ab49870d3ebdff
Andrew Adare <andrewadare@gmail.com>
2014-07-07 16:13:03 -0600

    Refactor

--- CalcBeamCenter.C
+++ CalcBeamCenter.C
@@ -1,35 +1,52 @@
-#include "GLSFitter.h"
-#include "UtilFns.h"
+#include "SvxTGeo.h"
 #include "SvxGeoTrack.h"
+#include "UtilFns.h"
+#include "BeamCenterFuncs.h"
+#include "ParameterDefs.h"
+#include "GLSFitter.h"
+#include "VtxIO.h"
 
-typedef vector<SvxGeoTrack> geoTracks;
+#include <TEllipse.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TGeoManager.h>
 
 // Solve the linear system y0[i] = -m[i]*bc0 + bc1 for i..ntrk-1 tracks.
 // m[i] is the slope (tan(phi)), y0[i] is the y-intercept, and (bc0,bc1)
 // is the least-squares beam (x,y) position.
 
-// TVectorD BC(TFile *f, TNtuple *t, int ntrk, TString arm);
-// TVectorD IPVec(TVectorD &a, TVectorD &n, TVectorD &p);
 TGraph *DcaDist(TFile *f, TNtuple *t, TVectorD &bc, TString arm,
+                TH1D *hr=0, int ntracks=50000);
+TGraph *DcaDist(geoTracks &tracks, TVectorD &bc, TString arm, 
                 TH1D *hr=0, int ntracks=50000);
 
 void CalcBeamCenter()
 {
   // Initial declarations & assignments
-  TFile *f = new TFile("rootfiles/411768_cluster.1.root");
-  TNtuple *t = (TNtuple *)f->Get("trktree");
+  TFile *f = new TFile("rootfiles/411768_cluster.root");
+  TNtuple *t = (TNtuple *)f->Get("seg_clusntuple");
   gStyle->SetOptStat(0);
   int ntrk = 5000;
 
+  SvxTGeo *geo = new SvxTGeo;
+  geo->ReadParFile("geom/svxPISA-411768.par");
+  geo->MakeTopVolume(100, 100, 100);
+  geo->AddSensors();
+  geo->GeoManager()->CloseGeometry();
+
+  geoTracks tracks;
+  GetTracksFromTree(t, geo, tracks);
+  FitTracks(tracks);
+
   Printf("Computing east arm beam center...");
-  TVectorD bce = BC(f, t, ntrk, "east");
+  TVectorD bce = BeamCenter(tracks, 15000, "east");
   TH1D *he = new TH1D("he", "he", 100,0,0.5);
-  TGraph *ge = DcaDist(f,t,bce,"east",he);
+  TGraph *ge = DcaDist(tracks,bce,"east",he);
 
   Printf("Computing west arm beam center...");
-  TVectorD bcw = BC(f, t, ntrk, "west");
+  TVectorD bcw = BeamCenter(tracks, 15000, "west");
   TH1D *hw = new TH1D("hw", "hw", 100,0,0.5);
-  TGraph *gw = DcaDist(f,t,bcw,"west",hw);
+  TGraph *gw = DcaDist(tracks,bcw,"west",hw);
 
   // ---- ---- ---- ---- ---- ---- ----
   //                Plot
@@ -39,7 +56,7 @@ void CalcBeamCenter()
                          "Beam center DCA #color[861]{east}, #color[800]{west}");
   h->GetXaxis()->SetTitle("x [cm]");
   h->GetYaxis()->SetTitle("y [cm]");
-  TEllipse ell(0,0,0.2,0.2);
+  TEllipse ell(0,0,0.1,0.1);
   ell.Draw("same");
   SetGraphProps(ge,kNone,kNone,kAzure+1,kFullDotMedium);
   SetGraphProps(gw,kNone,kNone,kOrange,kFullDotMedium);
@@ -58,111 +75,16 @@ void CalcBeamCenter()
   cr->SetLogy();
   cr->Print("pdfs/dcar.pdf");
 
-  Printf("Fraction outside 2000um: %.3f (e) %.3f (w)",
-         1.0 - he->Integral(1,he->FindBin(0.199))/he->Integral(),
-         1.0 - hw->Integral(1,hw->FindBin(0.199))/hw->Integral());
+  Printf("Fraction outside 1000um: %.3f (e) %.3f (w)",
+         1.0 - he->Integral(1,he->FindBin(0.099))/he->Integral(),
+         1.0 - hw->Integral(1,hw->FindBin(0.099))/hw->Integral());
   return;
 }
-
-// void
-// GetData(TFile *f, TNtuple *t, TVectorD &y0, TMatrixD &M, TString arm)
-// {
-//   assert(y0.GetNrows()==M.GetNrows());
-//   assert(M.GetNcols()==2);
-
-//   TTreeReader r(t->GetName(), f);
-//   TTreeReaderValue<float> ty0(r, "y0");
-//   TTreeReaderValue<float> phi(r, "phi");
-
-//   int i=0;
-//   while (i<y0.GetNrows() && r.Next())
-//   {
-//     double m = TMath::Tan(*phi);
-//     bool east = (*phi > 0.5*TMath::Pi() && *phi < 1.5*TMath::Pi());
-//     if ((arm=="east" && east) || (arm=="west" && !east))
-//     {
-//       M(i, 0) = -m;
-//       M(i, 1) = 1;
-//       y0(i) = *ty0;
-//       i++;
-//     }
-//   }
-
-//   return;
-// }
-
-// TVectorD
-// BC(TFile *f, TNtuple *t, int ntrk, TString arm)
-// {
-//   TMatrixD M(ntrk,2);    // "Design matrix" containing track slopes
-//   TVectorD y0(ntrk);     // Vector of track y-intercept values
-//   TMatrixD L(ntrk,ntrk); // Covariance matrix for track fit parameters y0, m
-//   L.UnitMatrix();
-//   L *= 0.01;             // TODO: Get mean dm, dy0 from track fits
-//   TMatrixD cov(2,2);
-//   GetData(f,t,y0,M,arm);
-//   TVectorD bc = SolveGLS(M, y0, L, cov);
-//   Printf("%s x,y (%.3f +- %.3f, %.3f +- %.3f)",
-//          arm.Data(),
-//          bc(0), TMath::Sqrt(cov(0,0)),
-//          bc(1), TMath::Sqrt(cov(1,1)));
-//   cov.Print();
-//   return bc;
-// }
-
-// TVectorD
-// BC(geoTracks &tracks, int ntrk, TString arm)
-// {
-//   // Compute least-squares beam center from ntrk tracks.
-//   // Warning: TrackFitSResid() overwrites tracks[i].ds as a side effect.
-
-//   int n = TMath::Min(ntrk, (int)tracks.size());
-//   TMatrixD M(n,2);   // "Design matrix" containing track slopes
-//   TVectorD y0(n);    // Vector of track y-intercept values
-//   TMatrixD L(n,n);   // Covariance matrix for track fit parameters y0, m
-//   L.UnitMatrix();
-//   L *= 0.01;         // TODO: Get mean dm, dy0 from track fits
-//   TMatrixD cov(2,2); // Assigned in SolveGLS()
-
-//   for (int i=0; i<n; i++)
-//   {
-//     // Perform straight-line fit --> residuals, track parameters
-//     double pars[2] = {0}; /* y0, phi */
-//     TrackFitSResid(tracks[i], pars);
-//     double yint = pars[0], phi = pars[1];
-//     bool east = (phi > 0.5*TMath::Pi() && phi < 1.5*TMath::Pi());
-//     if ((arm=="east" && east) || (arm=="west" && !east))
-//     {
-//       M(i, 0) = -TMath::Tan(phi);
-//       M(i, 1) = 1;
-//       y0(i) = yint;
-//       i++;
-//     }
-//   }
-
-//   TVectorD bc = SolveGLS(M, y0, L, cov);
-//   Printf("%s x,y (%.3f +- %.3f, %.3f +- %.3f)",
-//          arm.Data(),
-//          bc(0), TMath::Sqrt(cov(0,0)),
-//          bc(1), TMath::Sqrt(cov(1,1)));
-//   cov.Print();
-//   return bc;
-// }
-
-// TVectorD
-// IPVec(TVectorD &a, TVectorD &n, TVectorD &p)
-// {
-//   // Compute impact parameter vector from point p to line x = a + tn
-//   // where
-//   // - x is a straight-line track trajectory
-//   // - a is a point on vector x (e.g. y-intercept point (0, y0))
-//   // - n is a unit vector directing x (e.g. (cos(phi), sin(phi)).
-//   return a - p - ((a - p)*n)*n;
-// }
 
 TGraph *
 DcaDist(TFile *f, TNtuple *t, TVectorD &bc, TString arm, TH1D *hr, int ntracks)
 {
+  // This function is no longer being used.
   TTreeReader r(t->GetName(), f);
   TTreeReaderValue<float> ty0(r, "y0");
   TTreeReaderValue<float> phi(r, "phi");
@@ -191,32 +113,28 @@ DcaDist(TFile *f, TNtuple *t, TVectorD &bc, TString arm, TH1D *hr, int ntracks)
   return g;
 }
 
-// void
-// FilterTracks(TFile *f, TNtuple *t, TVectorD &bce, TVectorD& bcw, TFile* outFile)
-// {
-//   TTreeReader r(t->GetName(), f);
-//   TTreeReaderValue<float> ty0(r, "y0");
-//   TTreeReaderValue<float> phi(r, "phi");
-//   TTreeReaderValue<float> tid(r, "trkid");
+TGraph *
+DcaDist(geoTracks &tracks, TVectorD &bc, TString arm, TH1D *hr, int ntracks)
+{
+  TGraph *g = new TGraph();
+  g->SetMarkerStyle(kFullDotMedium);
+  for (unsigned int i=0; i<tracks.size(); i++)
+  {
+    double phi = tracks[i].phi0;
+    double m = TMath::Tan(phi);
+    bool east = (phi > 0.5*TMath::Pi() && phi < 1.5*TMath::Pi());
+    if ((arm=="east" && east) || (arm=="west" && !east))
+    {
+      TVectorD a(2); a(1) = tracks[i].vy;
+      TVectorD n(2); n(0) = TMath::Cos(phi); n(1) = TMath::Sin(phi);
+      TVectorD d = IPVec(a,n,bc);  // d = a - bc - ((a - bc)*n)*n;
 
-//   while (r.Next())
-//   {
-//     double m = TMath::Tan(*phi);
-//     bool east = (*phi > 0.5*TMath::Pi() && *phi < 1.5*TMath::Pi());
-//     if ((arm=="east" && east) || (arm=="west" && !east))
-//     {
-//       TVectorD a(2); a(1) = *ty0;
-//       TVectorD n(2); n(0) = TMath::Cos(*phi); n(1) = TMath::Sin(*phi);
-//       TVectorD d = IPVec(a,n,bc);  // d = a - bc - ((a - bc)*n)*n;
-
-//     }
-//   }
-//   return g;
-// }
-
-// void
-// FilterTracks()
-// {
-//   return;
-// }
+      if (i<(unsigned int)ntracks)
+        g->SetPoint(i, d(0), d(1));
+      if (hr)
+        hr->Fill(TMath::Sqrt(d*d));
+    }
+  }
+  return g;
+}
 
