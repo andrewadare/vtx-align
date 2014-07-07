29e9024ab300a5afc53bb808b14267b9bc89fd5e
Andrew Adare <andrewadare@gmail.com>
2014-07-07 13:48:18 -0600

    Factored functions off to BeamCenterFuncs.h

--- CalcBeamCenter.C
+++ CalcBeamCenter.C
@@ -8,12 +8,12 @@ typedef vector<SvxGeoTrack> geoTracks;
 // m[i] is the slope (tan(phi)), y0[i] is the y-intercept, and (bc0,bc1)
 // is the least-squares beam (x,y) position.
 
-TVectorD BC(TFile *f, TNtuple *t, int ntrk, TString arm);
-TVectorD IPVec(TVectorD &a, TVectorD &n, TVectorD &p);
+// TVectorD BC(TFile *f, TNtuple *t, int ntrk, TString arm);
+// TVectorD IPVec(TVectorD &a, TVectorD &n, TVectorD &p);
 TGraph *DcaDist(TFile *f, TNtuple *t, TVectorD &bc, TString arm,
                 TH1D *hr=0, int ntracks=50000);
 
-void BeamCenter()
+void CalcBeamCenter()
 {
   // Initial declarations & assignments
   TFile *f = new TFile("rootfiles/411768_cluster.1.root");
@@ -64,101 +64,101 @@ void BeamCenter()
   return;
 }
 
-void
-GetData(TFile *f, TNtuple *t, TVectorD &y0, TMatrixD &M, TString arm)
-{
-  assert(y0.GetNrows()==M.GetNrows());
-  assert(M.GetNcols()==2);
+// void
+// GetData(TFile *f, TNtuple *t, TVectorD &y0, TMatrixD &M, TString arm)
+// {
+//   assert(y0.GetNrows()==M.GetNrows());
+//   assert(M.GetNcols()==2);
 
-  TTreeReader r(t->GetName(), f);
-  TTreeReaderValue<float> ty0(r, "y0");
-  TTreeReaderValue<float> phi(r, "phi");
+//   TTreeReader r(t->GetName(), f);
+//   TTreeReaderValue<float> ty0(r, "y0");
+//   TTreeReaderValue<float> phi(r, "phi");
 
-  int i=0;
-  while (i<y0.GetNrows() && r.Next())
-  {
-    double m = TMath::Tan(*phi);
-    bool east = (*phi > 0.5*TMath::Pi() && *phi < 1.5*TMath::Pi());
-    if ((arm=="east" && east) || (arm=="west" && !east))
-    {
-      M(i, 0) = -m;
-      M(i, 1) = 1;
-      y0(i) = *ty0;
-      i++;
-    }
-  }
+//   int i=0;
+//   while (i<y0.GetNrows() && r.Next())
+//   {
+//     double m = TMath::Tan(*phi);
+//     bool east = (*phi > 0.5*TMath::Pi() && *phi < 1.5*TMath::Pi());
+//     if ((arm=="east" && east) || (arm=="west" && !east))
+//     {
+//       M(i, 0) = -m;
+//       M(i, 1) = 1;
+//       y0(i) = *ty0;
+//       i++;
+//     }
+//   }
 
-  return;
-}
+//   return;
+// }
 
-TVectorD
-BC(TFile *f, TNtuple *t, int ntrk, TString arm)
-{
-  TMatrixD M(ntrk,2);    // "Design matrix" containing track slopes
-  TVectorD y0(ntrk);     // Vector of track y-intercept values
-  TMatrixD L(ntrk,ntrk); // Covariance matrix for track fit parameters y0, m
-  L.UnitMatrix();
-  L *= 0.01;             // TODO: Get mean dm, dy0 from track fits
-  TMatrixD cov(2,2);
-  GetData(f,t,y0,M,arm);
-  TVectorD bc = SolveGLS(M, y0, L, cov);
-  Printf("%s x,y (%.3f +- %.3f, %.3f +- %.3f)",
-         arm.Data(),
-         bc(0), TMath::Sqrt(cov(0,0)),
-         bc(1), TMath::Sqrt(cov(1,1)));
-  cov.Print();
-  return bc;
-}
+// TVectorD
+// BC(TFile *f, TNtuple *t, int ntrk, TString arm)
+// {
+//   TMatrixD M(ntrk,2);    // "Design matrix" containing track slopes
+//   TVectorD y0(ntrk);     // Vector of track y-intercept values
+//   TMatrixD L(ntrk,ntrk); // Covariance matrix for track fit parameters y0, m
+//   L.UnitMatrix();
+//   L *= 0.01;             // TODO: Get mean dm, dy0 from track fits
+//   TMatrixD cov(2,2);
+//   GetData(f,t,y0,M,arm);
+//   TVectorD bc = SolveGLS(M, y0, L, cov);
+//   Printf("%s x,y (%.3f +- %.3f, %.3f +- %.3f)",
+//          arm.Data(),
+//          bc(0), TMath::Sqrt(cov(0,0)),
+//          bc(1), TMath::Sqrt(cov(1,1)));
+//   cov.Print();
+//   return bc;
+// }
 
-TVectorD
-BC(geoTracks &tracks, int ntrk, TString arm)
-{
-  // Compute least-squares beam center from ntrk tracks.
-  // Warning: TrackFitSResid() overwrites tracks[i].ds as a side effect.
+// TVectorD
+// BC(geoTracks &tracks, int ntrk, TString arm)
+// {
+//   // Compute least-squares beam center from ntrk tracks.
+//   // Warning: TrackFitSResid() overwrites tracks[i].ds as a side effect.
 
-  int n = TMath::Min(ntrk, (int)tracks.size());
-  TMatrixD M(n,2);   // "Design matrix" containing track slopes
-  TVectorD y0(n);    // Vector of track y-intercept values
-  TMatrixD L(n,n);   // Covariance matrix for track fit parameters y0, m
-  L.UnitMatrix();
-  L *= 0.01;         // TODO: Get mean dm, dy0 from track fits
-  TMatrixD cov(2,2); // Assigned in SolveGLS()
+//   int n = TMath::Min(ntrk, (int)tracks.size());
+//   TMatrixD M(n,2);   // "Design matrix" containing track slopes
+//   TVectorD y0(n);    // Vector of track y-intercept values
+//   TMatrixD L(n,n);   // Covariance matrix for track fit parameters y0, m
+//   L.UnitMatrix();
+//   L *= 0.01;         // TODO: Get mean dm, dy0 from track fits
+//   TMatrixD cov(2,2); // Assigned in SolveGLS()
 
-  for (int i=0; i<n; i++)
-  {
-    // Perform straight-line fit --> residuals, track parameters
-    double pars[2] = {0}; /* y0, phi */
-    TrackFitSResid(tracks[i], pars);
-    double yint = pars[0], phi = pars[1];
-    bool east = (phi > 0.5*TMath::Pi() && phi < 1.5*TMath::Pi());
-    if ((arm=="east" && east) || (arm=="west" && !east))
-    {
-      M(i, 0) = -TMath::Tan(phi);
-      M(i, 1) = 1;
-      y0(i) = yint;
-      i++;
-    }
-  }
+//   for (int i=0; i<n; i++)
+//   {
+//     // Perform straight-line fit --> residuals, track parameters
+//     double pars[2] = {0}; /* y0, phi */
+//     TrackFitSResid(tracks[i], pars);
+//     double yint = pars[0], phi = pars[1];
+//     bool east = (phi > 0.5*TMath::Pi() && phi < 1.5*TMath::Pi());
+//     if ((arm=="east" && east) || (arm=="west" && !east))
+//     {
+//       M(i, 0) = -TMath::Tan(phi);
+//       M(i, 1) = 1;
+//       y0(i) = yint;
+//       i++;
+//     }
+//   }
 
-  TVectorD bc = SolveGLS(M, y0, L, cov);
-  Printf("%s x,y (%.3f +- %.3f, %.3f +- %.3f)",
-         arm.Data(),
-         bc(0), TMath::Sqrt(cov(0,0)),
-         bc(1), TMath::Sqrt(cov(1,1)));
-  cov.Print();
-  return bc;
-}
+//   TVectorD bc = SolveGLS(M, y0, L, cov);
+//   Printf("%s x,y (%.3f +- %.3f, %.3f +- %.3f)",
+//          arm.Data(),
+//          bc(0), TMath::Sqrt(cov(0,0)),
+//          bc(1), TMath::Sqrt(cov(1,1)));
+//   cov.Print();
+//   return bc;
+// }
 
-TVectorD
-IPVec(TVectorD &a, TVectorD &n, TVectorD &p)
-{
-  // Compute impact parameter vector from point p to line x = a + tn
-  // where
-  // - x is a straight-line track trajectory
-  // - a is a point on vector x (e.g. y-intercept point (0, y0))
-  // - n is a unit vector directing x (e.g. (cos(phi), sin(phi)).
-  return a - p - ((a - p)*n)*n;
-}
+// TVectorD
+// IPVec(TVectorD &a, TVectorD &n, TVectorD &p)
+// {
+//   // Compute impact parameter vector from point p to line x = a + tn
+//   // where
+//   // - x is a straight-line track trajectory
+//   // - a is a point on vector x (e.g. y-intercept point (0, y0))
+//   // - n is a unit vector directing x (e.g. (cos(phi), sin(phi)).
+//   return a - p - ((a - p)*n)*n;
+// }
 
 TGraph *
 DcaDist(TFile *f, TNtuple *t, TVectorD &bc, TString arm, TH1D *hr, int ntracks)
