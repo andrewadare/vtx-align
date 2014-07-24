37df1a8ef1b1347c51702aba63f94cee2f651f70
Andrew Adare <andrewadare@gmail.com>
2014-07-24 12:36:14 -0600

    Reduced number of regularized ladders to one

--- BadLadders.h
+++ BadLadders.h
@@ -17,16 +17,16 @@ Dead(int layer, int ladder)
 bool
 Locked(int layer, int ladder)
 {
-  if (layer==2 && ladder==15) return true; // 50%
-  if (layer==3 && ladder==2)  return true; // 65%
-  //if (layer==3 && ladder==12) return true; // 70%
-  if (layer==3 && ladder==13) return true; // 66%
-  if (layer==3 && ladder==22) return true; // Not dead, but problematic
+  // if (layer==2 && ladder==15) return true; // 50%
+  // if (layer==3 && ladder==2)  return true; // 65%
+  // //if (layer==3 && ladder==12) return true; // 70%
+  if (layer==3 && ladder==13) return true; // 66% // Behind a dead pixel ladder==15
+  // if (layer==3 && ladder==22) return true; // Not dead, but problematic
 
-  // if (layer==3 && ladder==9)  return true; // 66%
-  // if (layer==3 && ladder==15) return true; // 66%
-  // if (layer==3 && ladder==19) return true; // 68%
-  if (layer==3 && ladder==21) return true; // 50%
+  // // if (layer==3 && ladder==9)  return true; // 66%
+  // // if (layer==3 && ladder==15) return true; // 66%
+  // // if (layer==3 && ladder==19) return true; // 68%
+  // if (layer==3 && ladder==21) return true; // 50%
   return false;
 }
 
