8d73bdd782d0a9d644aa4c80c737dbdd578744e1
Andrew Adare <andrewadare@gmail.com>
2014-07-24 13:59:02 -0600

    Commenting/reformatting

--- run-alignment.py
+++ run-alignment.py
@@ -1,24 +1,32 @@
-import sys, os
+import sys
+import os
 
 narg = len(sys.argv)
 run = 411768
 iter = 0
 
-if narg==1:
-  pass # Use defaults above
-elif narg==2 and sys.argv[1].isdigit():
-  iter = sys.argv[1]
-elif narg==3 and sys.argv[1].isdigit() and sys.argv[2].isdigit():
-  run = sys.argv[1]
-  iter = sys.argv[2]
+# No args - use defaults above
+if narg == 1:
+    pass
+
+# One argument - iteration index
+elif narg == 2 and sys.argv[1].isdigit():
+    iter = sys.argv[1]
+
+# Two arguments - run number and iteration
+elif narg == 3 and sys.argv[1].isdigit() and sys.argv[2].isdigit():
+    run = sys.argv[1]
+    iter = sys.argv[2]
+
 else:
-  print("Usage: python run-alignment.py <run> <iter> "),
-  print("(run number is optional and iter = 0,1,2,...)")
-  sys.exit(0)
+    print("Usage: python run-alignment.py <run> <iter> "),
+    print("(run number is optional and iter = 0,1,2,...)")
+    sys.exit(0)
 
 root = 'time root -b -q'
-cmd1 = "{} 'VtxAlign.C+({},{})' >& logs/iter{}.log".format(root,run,iter,iter)
-cmd2 = "{} 'DrawResults.C({},{})'".format(root,run,iter)
+cmd1 = "{} 'VtxAlign.C+({},{})' >& logs/iter{}.log".format(root,
+                                                           run, iter, iter)
+cmd2 = "{} 'DrawResults.C({},{})'".format(root, run, iter)
 
 print('Executing ' + cmd1)
 os.system(cmd1)
