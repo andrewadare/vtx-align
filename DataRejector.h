2ee0245118e0673769304ceb4d992ae1424ab4c3
Andrew Adare <andrewadare@gmail.com>
2014-08-05 10:39:57 -0600

    Use histograms instead of arrays for quantile calculations

--- DataRejector.h
+++ DataRejector.h
@@ -14,7 +14,7 @@ FilterData(geoEvents &events,
            double maxdca = 0.2,
            double maxres_s = 0.1,
            double maxres_z = 0.1,
-           TNtuple* ntuple = 0);
+           TNtuple *ntuple = 0);
 
 void
 FilterData(geoEvents &events,
@@ -23,45 +23,39 @@ FilterData(geoEvents &events,
            double maxdca,
            double maxres_s,
            double maxres_z,
-           TNtuple* ntuple)
+           TNtuple *ntuple)
 {
+  int minmult = 10;
 
-  Printf("Event multiplicity scan...");
-  int minmult = 20;
-  int N = 0; // Pre-allocation size
+  // x-y vertex distributions
+  Printf("Filling mult/vertex distributions...");
+  double x0 = -1.0, y0 = -1.0, x1 = +1.0, y1 = +1.0;
+  TH2D *hve = new TH2D(Form("%s_ve",ntuple->GetName()), 
+                       Form("East vertex;x [cm];y [cm]"),
+                       500, x0, x1, 500, y0, y1);
+  TH2D *hvw = new TH2D(Form("%s_vw",ntuple->GetName()), 
+                       Form("West vertex;x [cm];y [cm]"),
+                       500, x0, x1, 500, y0, y1);
   for (unsigned int ev=0; ev<events.size(); ev++)
   {
-    int ne=0, nw=0;
     int mult = events[ev].size();
     if (mult >= minmult)
-      N++;
-    for (int t=0; t<mult; t++)
     {
-      SvxGeoTrack trk = events[ev][t];
-      if (East(trk.phi0)) ne++;
-      else nw++;
+      TVectorD ve = Vertex(events.at(ev), "east");
+      TVectorD vw = Vertex(events.at(ev), "west");
+      hve->Fill(ve(0), ve(1));
+      hvw->Fill(vw(0), vw(1));
     }
   }
-
-  Printf("Allocating vertex arrays...");
-  int nfilled = 0;
-  double vxe[N], vye[N], vze[N];  // Vertex from east arm
-  double vxw[N], vyw[N], vzw[N];  // Vertex from west arm
-  double dvx[N], dvy[N], dvz[N];  // West - east offsets
-
-  Printf("Filling vertex arrays...");
-  FillVertexArrays(events, minmult,
-                   vxe, vye, vze, vxw, vyw, vzw, dvx, dvy, dvz,
-                   nfilled);
 
   Printf("Computing quantiles for {e,w}{x,y}");
   const int nq = 5;
   double probs[nq] = {vertexprobmin, 0.32, 0.50, 0.68, vertexprobmax};
   double qxe[nq] = {0}, qxw[nq] = {0}, qye[nq] = {0}, qyw[nq] = {0};
-  TMath::Quantiles(nfilled, nq, vxe, qxe, probs, false);
-  TMath::Quantiles(nfilled, nq, vxw, qxw, probs, false);
-  TMath::Quantiles(nfilled, nq, vye, qye, probs, false);
-  TMath::Quantiles(nfilled, nq, vyw, qyw, probs, false);
+  hve->ProjectionX()->GetQuantiles(nq, qxe, probs);
+  hvw->ProjectionX()->GetQuantiles(nq, qxw, probs);
+  hve->ProjectionY()->GetQuantiles(nq, qye, probs);
+  hvw->ProjectionY()->GetQuantiles(nq, qyw, probs);
 
   TVectorD bce(2); bce(0) = qxe[2]; bce(1) = qye[2];
   TVectorD bcw(2); bcw(0) = qxw[2]; bcw(1) = qyw[2];
