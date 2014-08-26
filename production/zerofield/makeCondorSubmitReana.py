30c0372e3ae9cf71e5600cc771c90239a00f60cc
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-08-26 17:07:33 -0400

    Macros for re-running the analysis of production DST's

---
+++ production/zerofield/makeCondorSubmitReana.py
@@ -0,0 +1,75 @@
+#!/opt/phenix/bin/python
+'''
+Makes condor submit file for zf alignment production
+running the analysis portion over previous production
+output.
+'''
+
+import os
+import sys
+from argparse import ArgumentParser
+
+#First get any command line arguments
+parser = ArgumentParser(description = """Make condor submit script
+										 for zerofield alignment production""")
+
+parser.add_argument("-f", "--file", dest="filename",
+                  help="write output to FILE", metavar="FILE")
+parser.add_argument("-r", "--run", dest="run", help="run number")
+parser.add_argument("-n", "--nsegments", dest="nsegments", help="number of segments")
+parser.add_argument("-o", "--output", dest="outdir",
+					help="Write output to DIR", metavar="DIR")
+parser.add_argument("-v", "--verbose",
+                  action="store_true", dest="verbose", default=True,
+                  help="print status messages to stdout")
+
+parser.set_defaults(filename="condor.job",
+					config="/direct/phenix+u/dcm07e/work/vtx-align/production/config/config-ideal.txt",
+					run='406541',
+					nsegments='24',
+					outdir="/direct/phenix+prod01/phnxreco/millepede/test/")
+
+args = parser.parse_args()
+
+
+
+#Check the input arguments for compatabiity and fix if necessary
+if not args.outdir.endswith('/'):
+	args.outdir += '/'
+
+if not os.path.isfile(args.config):
+	print("\nERROR!! Can't find config file - {}\n".format(args.config))
+	sys.exit(1)
+
+
+#Print out arguments
+if args.verbose :
+	print(" Writting job file to {}".format(args.filename))
+	print(" Using run {}".format(args.run))
+	print(" Producing {} segments".format(args.nsegments))
+	print(" Output of condor jobs will go to {}".format(args.outdir))
+
+#Check whether the output directory specified exists
+# if not, create it
+os.system("mkdir -p {}condor_logs".format(args.outdir))
+
+
+#make the file
+fo = open(args.filename,'w')
+
+#write the job information
+fo.write("Universe        = vanilla\n")
+fo.write("Executable      = {}/run_reanalysis.py\n".format(os.getcwd()))
+fo.write("Arguments       = {} $(Process) {} {}\n".format(args.run,args.outdir,os.getcwd()[0:-20]))
+fo.write("Requirements    = (CPU_Speed >= 1 && CPU_Experiment == \"phenix\")\n")
+fo.write("Rank            = CPU_Speed\n")
+fo.write("Priority        = +1\n")
+fo.write("GetEnv          = True\n")
+fo.write("Initialdir      = {}\n".format(os.getcwd()))
+fo.write("Output          = {}condor_logs/ran_reana_{:0>10}-$(Process).out\n".format(args.outdir,args.run,))
+fo.write("Error           = {}condor_logs/ran_reana_{:0>10}-$(Process).err\n".format(args.outdir,args.run,))
+fo.write("Log             = {}condor_logs/ran_reana_{:0>10}-$(Process).log\n".format(args.outdir,args.run,))
+fo.write("+Experiment     = \"phenix\"\n")
+fo.write("PeriodicHold = (NumJobStarts >= 1 && JobStatus == 1)\n")
+fo.write("+Job_Type       = \"cas\"\n")
+fo.write("Queue {}".format(args.nsegments))
---
+++ production/zerofield/run_reanalysis.py
@@ -0,0 +1,111 @@
+#!/opt/phenix/bin/python
+'''
+Python script for re-analyzing the DST's produced
+from a previous production. Overwrites previous
+ana output.
+'''
+
+import os
+import sys
+import stat
+
+##############################################
+# Get the command line arguments
+##############################################
+runNumber = int(sys.argv[1])
+segNumber = int(sys.argv[2])
+outDir = sys.argv[3]
+vtxalignDir = sys.argv[4]
+
+##############################################
+# Set some parameters for running
+##############################################
+nevents = 0
+productionDir = vtxalignDir + "production/zerofield/"
+
+
+
+##############################################
+# Set environment variables
+##############################################
+print("\n--> Setting environment variables")
+
+#os.environ["ODBCINI"] = "/opt/phenix/etc/odbc.ini.master"
+#print("ODBCINI: {}".format(os.environ["ODBCINI"]))
+
+
+# get the name of the dst
+filenum = "{:0>10}-{:0>4}.root".format(runNumber,segNumber)
+outfiles = os.listdir("{}DST_SVX/".format(outDir))
+for file in outfiles:
+	if str(filenum) in file:
+		dstFile = "{}DST_SVX/{}".format(outDir,file)
+		break
+print("Found DST: {}".format(dstFile))
+
+##############################################
+# run over the data, producing the ntuples
+##############################################
+print("\n--> Running over the DST to produce cluster ntuple")
+
+os.chdir(outDir)
+os.system("mkdir -p ntuple")
+os.system("mkdir -p ntuple/anavtxcluster")
+os.chdir(productionDir)
+print("current directory: " + os.getcwd())
+
+ntupleFile = "{}ntuple/anavtxcluster/anavtxcluster_{:0>10}-{:0>4}.root".format(outDir,runNumber,segNumber)
+print("Output Ntuple: {}".format(ntupleFile))
+
+command = "root -b -q \'run_anavtxcluster.C("
+command += str(nevents) + ","
+command += "\"" + dstFile + "\","
+command += "\"" + ntupleFile
+command += ")\'"
+print(command)
+
+os.system(command)
+
+##############################################                                                                                                                                                                                                             
+# run over the data, producing the ntuples                                                                                                                                                                                                                 
+##############################################                                                                                                                                                                                                             
+print("\n--> Running over the DST to produce cluster ntuple")
+
+os.chdir(outDir)
+os.system("mkdir -p ntuple")
+os.system("mkdir -p ntuple/testvtxproduction")
+os.chdir(productionDir)
+print("current directory: " + os.getcwd())
+
+ntupleFile = "{}ntuple/testvtxproduction/testvtxproduction_{:0>10}-{:0>4}.root".format(outDir,runNumber,segNumber)
+print("Output Ntuple: {}".format(ntupleFile))
+
+command = "root -b -q \'analyze_run14_test_prod_output.C("
+command += str(nevents) + ","
+command += "\"" + dstFile + "\","
+command += "\"" + ntupleFile
+command += ")\'"
+print(command)
+
+os.system(command)
+
+##############################################
+# cleanup
+##############################################
+print("\n--> Cleaning up.")
+
+#make all the directories we were in group writable
+os.system("chmod a+w {}".format(outDir))
+for root, dirs, files in os.walk(outDir, topdown=False):
+        for dir in dirs:
+            os.system("chmod a+w {}".format(outDir+dir))
+
+##############################################
+# DONE!
+##############################################
+
+print("\n---- DONE! ----\n")
+
+
+
+
