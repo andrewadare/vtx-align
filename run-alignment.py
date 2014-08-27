4315e82df27292166453911a21a953eb1a004be5
Andrew Adare <andrewadare@gmail.com>
2014-08-27 15:50:16 -0600

    Run CalcBeamCenter() before VtxAlign() so beam center is available.

--- run-alignment.py
+++ run-alignment.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-on = True  # enable/disable execution
+on = True # enable/disable execution
 run = 411768
-p = 0  # production step
-itersteps = [0,1,2]
+p = 6  # production step
+itersteps = [20, 21, 22]
 
 pat = '{}-{}-{}'.format(run, p, 0)
 g = 'geom/{}.par'.format(pat)
@@ -19,18 +19,25 @@ if os.path.isfile(r) == False:
     print('Error: {} not found.'.format(g))
     sys.exit()
 
-for i in itersteps[:-1]:
-    m = "'VtxAlign.C+({},{},{})' >& logs/{}-{}-{}.log".format(run, p, i, 
-                                                              run, p, i)
-    print('Executing ' + root + m)
-    if on: os.system(root + m)
-
-for i in itersteps[:-1]:
-    m = "'DrawResults.C+({},{},{},{},{})'".format(run, p, i, p, i+1)
-    print('Executing ' + root + m)
-    if on: os.system(root + m)
-
 for i in itersteps:
+
+    # Run CalcBeamCenter.C
     m = "'CalcBeamCenter.C+({},{},{})'".format(run, p, i)
     print('Executing ' + root + m)
-    if on: os.system(root + m)
+    if on:
+        os.system(root + m)
+
+    if i != itersteps[-1]:
+        
+        # Run VtxAlign.C
+        m = "'VtxAlign.C+({},{},{})' >& logs/{}-{}-{}.log".format(run, p, i,
+                                                                  run, p, i)
+        print('Executing ' + root + m)
+        if on:
+            os.system(root + m)
+
+        # Run DrawResults.C
+        m = "'DrawResults.C+({},{},{},{},{})'".format(run, p, i, p, i + 1)
+        print('Executing ' + root + m)
+        if on:
+            os.system(root + m)
