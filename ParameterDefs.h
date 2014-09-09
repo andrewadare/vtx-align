05ed0510cd7c7b5b50b2db2b10cc024e15fea729
Andrew Adare <andrewadare@gmail.com>
2014-09-09 09:46:41 -0600

    Added new coordinates and parameter labeling scheme

--- ParameterDefs.h
+++ ParameterDefs.h
@@ -9,7 +9,7 @@ void ParInfo(int label, int &layer, int &ladder, string &coord);
 void HalfLayerParInfo(int label, int &layer, int &arm, string &coord);
 
 int
-Label(int layer, int ladder, string &coord)
+Label(int layer, int ladder, string coord)
 {
   // Return a global parameter label for this layer, ladder, and coordinate.
   // In Millepede II, any unique integer > 0 will do.
@@ -17,15 +17,14 @@ Label(int layer, int ladder, string &coord)
   int start[4] = {0,10,30,46}; // # ladders within & excluding layer 0,1,2,3
   int ic = 99;
 
-  if (coord.compare("s") == 0)
-    ic = 0;
-  if (coord.compare("z") == 0)
-    ic = 1;
-  if (coord.compare("r") == 0)
-    ic = 2;
-
-  // Add other coordinates / degrees of freedom here as needed
-  // Then ParInfo() would need corresponding modification.
+  // Ladder coordinate index map. Warnings:
+  // 1. Accessing a non-existing element creates it! 
+  //    Always condition on find(key).
+  // 2. This map is duplicated in ParInfo() [TODO: (low priority) improve.]
+  //    Any edits to this map must be copied there.
+  map<string, int> lci = {{"x",0},{"y",1},{"z",2},{"s",3},{"r",4}};
+  if (lci.find(coord) != lci.end())
+    ic = lci[coord];
 
   return 70*ic + start[layer] + ladder + 1;
 }
@@ -72,7 +71,12 @@ ParInfo(int label, int &layer, int &ladder, string &coord)
   ladder = l - start[layer] - 1;
 
   // Assign coord
-  coord = (label <= 70)? "s" : (label <= 140)? "z" : (label <= 210)? "r" : "";
+  int ic = (label - start[layer] - ladder - 1)/70;
+  map<int,string> lcmap = {{0,"x"},{1,"y"},{2,"z"},{3,"s"},{4,"r"}};
+  if (lcmap.find(ic) != lcmap.end())
+    coord = lcmap[ic];
+  else
+    coord = "";
 
   return;
 }
@@ -80,7 +84,7 @@ ParInfo(int label, int &layer, int &ladder, string &coord)
 void
 HalfLayerParInfo(int label, int &layer, int &arm, string &coord)
 {
-  const char* coords[4] = {"x", "y", "z", "phi"};
+  const char *coords[4] = {"x", "y", "z", "phi"};
 
   layer = (label%1000 - label%100)/100;
   arm   = (label%100 - label%10)/10;
