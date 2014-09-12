bfe862dc96bcb77b201841fa3e8fdf3e699d4746
Andrew Adare <andrewadare@gmail.com>
2014-09-12 14:26:46 -0600

    Write par file along with simulated data

--- GenerateEvents.C
+++ GenerateEvents.C
@@ -3,9 +3,12 @@
 
 void GenerateEvents()
 {
+  int run = 123456;
+  int prod = 0;
   const int nevents = (int)1e4;
   SvxTGeo *tgeo = VTXModel("geom/svxPISA-ideal.par");
-  TString rootFileOut = Form("rootfiles/%d-%d-%d.root", 123456, 0, 0);
+  TString rootFileOut = Form("rootfiles/%d-%d-%d.root", run, prod, 0);
+  TString pisaFileOut = Form("geom/%d-%d-%d.par", run, prod, 0);
 
   Printf("Generating %d events...", nevents);
   geoEvents events(nevents);
@@ -25,11 +28,23 @@ void GenerateEvents()
 
   Printf("%lu events generated.", events.size());
 
+  // Misalign half-layer 1W.
+  // Then update global hit positions to reflect misalignments.
+  // Local hit positions (x,z on sensor) remain unchanged.
+  tgeo->TranslateHalfLayer(1, 1, 0.01, 0.01, 0.);
+  for (unsigned int ev=0; ev<events.size(); ev++)
+    for (unsigned int t=0; t<events[ev].size(); t++)
+      events[ev][t].UpdateHits();
+
   // Here, fit tracks to (re)assign residuals, angles, and intercepts.
   // This puts the tracks on the same footing as the post-aligned refit sample.
-  // Without this step, a comparison of residuals before and after alignment 
+  // Without this step, a comparison of residuals before and after alignment
   // would not be apples-to-apples.
   FitTracks(events);
+
+  // Write out (misaligned) geometry to par file
+  Printf("Writing %s", pisaFileOut.Data());
+  tgeo->WriteParFile(pisaFileOut.Data());
 
   cout << "Filling output tree(s)..." << flush;
   TFile *outFile = new TFile(rootFileOut.Data(), "recreate");
