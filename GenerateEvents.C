86f444aaf66fcf7c8f912e4496f2b05024fb553b
Andrew Adare <andrewadare@gmail.com>
2014-09-16 12:17:15 -0600

    Generate hybrid VTX/external tracks to simulate SvxCentralTracks

--- GenerateEvents.C
+++ GenerateEvents.C
@@ -5,16 +5,19 @@ void GenerateEvents()
 {
   int run = 123456;
   int prod = 0;
-  const int nevents = (int)1e4;
+  const int nvtxevents = (int)9e3;
+  const int ncntevents = (int)1e3;
   SvxTGeo *tgeo = VTXModel("geom/svxPISA-ideal.par");
   TString rootFileOut = Form("rootfiles/%d-%d-%d.root", run, prod, 0);
   TString pisaFileOut = Form("geom/%d-%d-%d.par", run, prod, 0);
 
-  Printf("Generating %d events...", nevents);
-  geoEvents events(nevents);
+  geoEvents vtxevents(nvtxevents);
+  geoEvents cntevents(ncntevents);
   TRandom3 ran;
   TVectorD vertex(3);
-  for (int n=0; n<nevents; n++)
+
+  Printf("Generating %d VTX standalone events...", nvtxevents);
+  for (int n=0; n<nvtxevents; n++)
   {
     vertex(0) = 0.010*ran.Gaus();
     vertex(1) = 0.010*ran.Gaus();
@@ -23,24 +26,43 @@ void GenerateEvents()
     geoTracks tracks;
     int ntracks = 1 + ran.Integer(50);
     GenerateEvent(tgeo, ntracks, vertex, tracks);
-    events[n] = tracks;
+    vtxevents[n] = tracks;
   }
+  Printf("%lu VTX events generated.", vtxevents.size());
 
-  Printf("%lu events generated.", events.size());
+  // Generate fake SvxCentralTracks
+  Printf("Generating %d CNT events...", ncntevents);
+  for (int n=0; n<ncntevents; n++)
+  {
+    vertex(0) = 0.010*ran.Gaus();
+    vertex(1) = 0.010*ran.Gaus();
+    vertex(2) = 2*ran.Gaus();
 
-  // Misalign half-layer 1W.
-  // Then update global hit positions to reflect misalignments.
+    geoTracks tracks;
+    int ntracks = 1 + ran.Integer(50);
+    GenerateEvent(tgeo, ntracks, vertex, tracks);
+    cntevents[n] = tracks;
+  }
+  Printf("%lu CNT events generated.", cntevents.size());
+
+  // ** Apply misalignments here **
+  tgeo->TranslateHalfLayer(1, 0, -0.02, 0.0, 0.0);
+
+  // Update global hit positions to reflect misalignments.
   // Local hit positions (x,z on sensor) remain unchanged.
-  tgeo->TranslateHalfLayer(1, 1, 0.01, 0.01, 0.);
-  for (unsigned int ev=0; ev<events.size(); ev++)
-    for (unsigned int t=0; t<events[ev].size(); t++)
-      events[ev][t].UpdateHits();
+  for (unsigned int ev=0; ev<vtxevents.size(); ev++)
+    for (unsigned int t=0; t<vtxevents[ev].size(); t++)
+      vtxevents[ev][t].UpdateHits();
+  for (unsigned int ev=0; ev<cntevents.size(); ev++)
+    for (unsigned int t=0; t<cntevents[ev].size(); t++)
+      cntevents[ev][t].UpdateHits();
 
-  // Here, fit tracks to (re)assign residuals, angles, and intercepts.
+  // Fit VTX tracks to (re)assign residuals, angles, and intercepts.
   // This puts the tracks on the same footing as the post-aligned refit sample.
   // Without this step, a comparison of residuals before and after alignment
   // would not be apples-to-apples.
-  FitTracks(events);
+  // CNT tracks are not fit here--their parameters are determined externally. 
+  FitTracks(vtxevents);
 
   // Write out (misaligned) geometry to par file
   Printf("Writing %s", pisaFileOut.Data());
@@ -48,8 +70,10 @@ void GenerateEvents()
 
   cout << "Filling output tree(s)..." << flush;
   TFile *outFile = new TFile(rootFileOut.Data(), "recreate");
-  TNtuple *ht = new TNtuple("vtxhits", "VTX hit variables", HITVARS);
-  FillNTuple(events, ht);
+  TNtuple *vtxhits = new TNtuple("vtxhits", "VTX hit variables", HITVARS);
+  TNtuple *cnthits = new TNtuple("cnthits", "CNT hit variables", HITVARS);
+  FillNTuple(vtxevents, vtxhits);
+  FillNTuple(cntevents, cnthits);
   Printf("done.");
   Printf("Writing %s", rootFileOut.Data());
   outFile->cd();
