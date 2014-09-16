4630ec1b3c18df13e8827b30e85a060520119fe2
Andrew Adare <andrewadare@gmail.com>
2014-09-16 12:15:22 -0600

    Commit this for now; will uncomment bad ladders when working with real data again

--- BadLadders.h
+++ BadLadders.h
@@ -7,6 +7,7 @@ bool Locked(int layer, int ladder);
 bool
 Dead(int layer, int ladder)
 {
+  return false; // TEMP - simulated data
   if (layer==1 && ladder==11) return true;
   if (layer==3 && ladder==10) return true;
   if (layer==3 && ladder==16) return true;
@@ -17,16 +18,8 @@ Dead(int layer, int ladder)
 bool
 Locked(int layer, int ladder)
 {
-  // if (layer==2 && ladder==15) return true; // 50%
-  // if (layer==3 && ladder==2)  return true; // 65%
-  // //if (layer==3 && ladder==12) return true; // 70%
+  return false; // TEMP - simulated data
   if (layer==3 && ladder==13) return true; // 66% // Behind a dead pixel ladder==15
-  // if (layer==3 && ladder==22) return true; // Not dead, but problematic
-
-  // // if (layer==3 && ladder==9)  return true; // 66%
-  // // if (layer==3 && ladder==15) return true; // 66%
-  // // if (layer==3 && ladder==19) return true; // 68%
-  // if (layer==3 && ladder==21) return true; // 50%
   return false;
 }
 
