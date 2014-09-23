2411efe809d2518e30b6c6c12e84283780b99a2b
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-09-23 11:55:43 -0600

    test the event syncing

--- test_eventsync.C
+++ test_eventsync.C
@@ -2,54 +2,93 @@
 #include "VtxIO.h"
 #include <utility>
 #include <map>
+#include <iomanip>
 
 using namespace std;
 
-void test_eventsync()
+void test_eventsync(const char *inFile = "rootfiles/anavtxcluster_0000411768-0000.root",
+                    const char *geoFile = "geom/411768-0-0.par",
+                    int nevents = 8)
 {
 
-  int ntracks = 1000;
+    //-- Get ntuples from file
+    TFile *fin = TFile::Open(inFile);
+    assert(fin);
 
-  TFile *inFile = new TFile("rootfiles/411768-0-0_3.root", "read");
+    TNtuple *segclus = (TNtuple *) fin->Get("seg_clusntuple");
+    TNtuple *cntclus = (TNtuple *) fin->Get("cnt_clusntuple");
+    assert(segclus);
+    assert(cntclus);
 
-  SvxTGeo *tgeo = VTXModel("geom/411768-0-0.par");
-
-  TNtuple *svxseg = (TNtuple *)inFile->Get("vtxhits");
-  assert(svxseg);
-
-  TNtuple *svxcnt = (TNtuple *)inFile->Get("cnthits");
-  assert(svxcnt);
-
-
-  multimap<int, int> seg_eventsync;
-  geoTracks seg_tracks;
-
-  GetTracksFromTree(svxseg, tgeo, seg_tracks, seg_eventsync , ntracks, "");
-
-  geoTracks cnt_tracks;
-  multimap<int, int> cnt_eventsync;
-
-  GetTracksFromTree(svxcnt, tgeo, cnt_tracks, cnt_eventsync , ntracks, "");
+    SvxTGeo *tgeo = VTXModel(geoFile);
 
 
-  for(unsigned int i = 0; i < seg_tracks.size();i++)//loop thru standalone tracks
+    //-- Read events from tree
+    cout << endl;
+    cout << "--> Getting unsynced events from ntuple" << endl;
+    geoEvents segevt;
+    map<int, int> segmap;
+    GetEventsFromClusTree(segclus, tgeo, segevt, segmap, nevents);
+    cout << "  n seg events = " << segevt.size() << endl;
+
+    geoEvents cntevt;
+    map<int, int> cntmap;
+    GetEventsFromClusTree(cntclus, tgeo, cntevt, cntmap, nevents, "cnt");
+    cout << "  n cnt events = " << cntevt.size() << endl;
+
+    //-- Print a table of the number of tracks / event
+    cout << endl;
+    cout << "--> # of tracks per event" << endl;
+    cout << setw(6) << "event"
+         << setw(7) << "index"
+         << setw(6) << "Nseg"
+         << setw(6) << "Ncnt"
+         << endl;
+    map<int, int>::iterator cntpos;
+    map<int, int>::iterator segpos;
+    for (segpos = segmap.begin(); segpos != segmap.end(); ++segpos)
     {
-      multimap<int,int>::iterator it = seg_eventsync.find(i);
-      int event = (*it).second;
+        cout << setw(6) << segpos->first
+             << setw(7) << segpos->second
+             << setw(6) << segevt[segpos->second].size();
 
-      for(unsigned int j = 0; j < cnt_tracks.size();j++)//loop thru cnt tracks
-	{
-	  multimap<int,int>::iterator it2 = cnt_eventsync.find(j);
-	  if(event!=(*it2).second)//check to see if they have the same event number
-	    {
-	      continue;
-	    }
-	  else
-	    {
-	      //do stuff here
-	    }
-	}
+        cntpos = cntmap.find(segpos->first);
+        if (cntpos != cntmap.end())
+        {
+            cout << setw(6) << cntevt[cntpos->second].size();
+        }
+
+        cout << endl;
+
+
     }
+
+    //-- Get Synced events from tree
+    cout << "--> Getting synced events from ntuples" << endl;
+    geoEvents segsyncevt;
+    geoEvents cntsyncevt;
+    GetSyncedEventsFromClusTree(segclus, cntclus, segsyncevt, cntsyncevt, tgeo, nevents);
+
+    cout << "  n seg events = " << segsyncevt.size() << endl;
+    cout << "  n cnt events = " << cntsyncevt.size() << endl;
+
+    cout << endl;
+    cout << "--> # of tracks per synced event" << endl;
+    cout << setw(6) << "event"
+         << setw(7) << "index"
+         << setw(6) << "Nseg"
+         << setw(6) << "Ncnt"
+         << endl;
+    for (int i = 0; i < segsyncevt.size(); i++)
+    {
+        cout << setw(6) << i
+             << setw(7) << i
+             << setw(6) << segsyncevt.at(i).size()
+             << setw(6) << cntsyncevt.at(i).size()
+             << endl;
+    }
+
+
 
 
 }
