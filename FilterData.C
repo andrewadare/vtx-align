08b18554a8fddf2fbffd2cbbef9c2691c04d4e54
Theodore Koblesky <theok@rcas2070.rcf.bnl.gov>
2014-09-23 07:39:23 -0400

    now filling cnt ntuple

--- FilterData.C
+++ FilterData.C
@@ -8,9 +8,9 @@
 // run-prod-iter.par so the base matches the ROOT file.
 // The alignment script VtxAlign.C requires a .par file with such a name.
 
-void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro6.root",
-                const char *outfilename = "rootfiles/411768-6-20.root",
-                const char *pisafilename = "geom/411768-6-20.par",
+void FilterData(const char *infilename = "rootfiles/zf-411768-0-0_3.root",
+                const char *outfilename = "rootfiles/411768-0-0.root",
+                const char *pisafilename = "geom/411768-0-0.par",
                 double vertexprobmin = 0.02,
                 double vertexprobmax = 0.98,
                 double maxdca = 0.5,
@@ -23,14 +23,22 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro6.ro
   TNtuple *svxseg = (TNtuple *)inFile->Get("seg_clusntuple");
   assert(svxseg);
 
+  TNtuple *svxcnt = (TNtuple *)inFile->Get("cnt_clusntuple");
+  assert(svxcnt);
+
   TFile *outFile = new TFile(outfilename, "recreate");
   TNtuple *vtxhits = new TNtuple("vtxhits", "VTX hit variables", HITVARS);
-  // TNtuple *cnthits = new TNtuple("cnthits", "CNT hit variables", HITVARS);
+  TNtuple *cnthits = new TNtuple("cnthits", "CNT hit variables", HITVARS);
 
   SvxTGeo *tgeo = VTXModel(pisafilename);
 
   geoEvents vtxevents;
+  geoEvents vtxevents2;
+
   GetEventsFromTree(svxseg, tgeo, vtxevents, nevents);
+
+  GetEventsFromTree(svxcnt, tgeo, vtxevents2, nevents,"cnt");
+
   FitTracks(vtxevents);
   FilterData(vtxevents,
              vertexprobmin,
@@ -39,6 +47,8 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro6.ro
              maxres_s,
              maxres_z,
              vtxhits);
+
+  FillNtuple(vtxevents2,cnthits);
 
   Printf("%.1f%% of initial data rejected.",
          100*(1.0 - (float)vtxhits->GetEntries()/svxseg->GetEntries()));
