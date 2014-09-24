1811233624b331bb7af080474e96d22b7c237fe0
Andrew Adare <andrewadare@gmail.com>
2014-09-24 15:56:09 -0600

    A few compiler warnings + reindent

--- FilterData.C
+++ FilterData.C
@@ -10,7 +10,7 @@
 // run-prod-iter.par so the base matches the ROOT file.
 // The alignment script VtxAlign.C requires a .par file with such a name.
 
-void FilterData(const char *infilename = "rootfiles/zf-411768-0-0_3.root",
+void FilterData(const char *infilename = "rootfiles/zf-411768-0-0_3-anavtxcluster.root",
                 const char *outfilename = "rootfiles/411768-0-0.root",
                 const char *pisafilename = "geom/411768-0-0.par",
                 double vertexprobmin = 0.02,
@@ -18,94 +18,86 @@ void FilterData(const char *infilename = "rootfiles/zf-411768-0-0_3.root",
                 double maxdca = 0.5,
                 double maxres_s = 0.1,
                 double maxres_z = 0.1,
-                int nevents = -1, // -1 = everything
+                int nevents = 300000, // -1 = everything
                 TString opt = "cnt")
 {
-    std::cout << "-- Opening " << infilename << " --" << std::endl;
-    TFile *inFile = new TFile(infilename, "read");
+  std::cout << "-- Opening " << infilename << " --" << std::endl;
+  TFile *inFile = new TFile(infilename, "read");
 
-    std::cout << "-- Reading seg_clusntuple --" << std::endl;
-    TNtuple *svxseg = (TNtuple *)inFile->Get("seg_clusntuple");
-    TNtuple *svxcnt;
-    assert(svxseg);
+  std::cout << "-- Reading seg_clusntuple --" << std::endl;
+  TNtuple *svxseg = (TNtuple *)inFile->Get("seg_clusntuple");
+  TNtuple *svxcnt;
+  assert(svxseg);
 
+  std::cout << "-- Initializing SvxTGeo --" << std::endl;
+  SvxTGeo *tgeo = VTXModel(pisafilename);
 
-    std::cout << "-- Initializing SvxTGeo --" << std::endl;
-    SvxTGeo *tgeo = VTXModel(pisafilename);
+  std::cout << "-- Creating output file " << outfilename << " --" << std::endl;
+  TFile *outFile = new TFile(outfilename, "recreate");
+  // TNtuple *vtxhits = new TNtuple("vtxhits", "VTX hit variables", HITVARS);
+  // TNtuple *cnthits = new TNtuple("cnthits", "CNT hit variables", HITVARS);
 
+  std::cout << "-- Creating vtxtrks Tree --" << std::endl;
+  TTree *vtxtrks = CreateTree("vtxtrks");
+  TTree *cnttrks;
 
-    std::cout << "-- Creating output file " << outfilename << " --" << std::endl;
-    TFile *outFile = new TFile(outfilename, "recreate");
-    // TNtuple *vtxhits = new TNtuple("vtxhits", "VTX hit variables", HITVARS);
-    // TNtuple *cnthits = new TNtuple("cnthits", "CNT hit variables", HITVARS);
+  geoEvents vtxevents;
+  geoEvents cntevents;
+
+  if (opt.Contains("cnt"))
+  {
+    std::cout << "-- Reading cnt_clusntuple --" << std::endl;
+    svxcnt = (TNtuple *) inFile->Get("cnt_clusntuple");
+    assert(svxcnt);
 
     std::cout << "-- Creating vtxtrks Tree --" << std::endl;
-    TTree *vtxtrks = CreateTree("vtxtrks");
-    TTree *cnttrks;
+    cnttrks = CreateTree("cnttrks");
 
+    GetSyncedEventsFromClusTree(svxseg, svxcnt,
+                                vtxevents, cntevents,
+                                tgeo, nevents);
 
-    geoEvents vtxevents;
-    geoEvents cntevents;
+    //need to fit segments to get phi, theta
+    std::cout << "-- Fitting tracks --" << std::endl;
+    FitTracks(vtxevents);
 
-    if (opt.Contains("cnt"))
-    {
-        std::cout << "-- Reading cnt_clusntuple --" << std::endl;
-        svxcnt = (TNtuple *) inFile->Get("cnt_clusntuple");
-        assert(svxcnt);
+    std::cout << "-- Filtering data --" << std::endl;
+    FilterData(vtxevents, cntevents,
+               vtxtrks, cnttrks,
+               vertexprobmin,
+               vertexprobmax,
+               maxdca,
+               maxres_s,
+               maxres_z);
+  }
+  else
+  {
+    std::cout << "-- Reading events from ntuple --" << std::endl;
+    map <int, int> tmpmap;
+    GetEventsFromClusTree(svxseg, tgeo, vtxevents, tmpmap, nevents);
 
-        std::cout << "-- Creating vtxtrks Tree --" << std::endl;
-        cnttrks = CreateTree("cnttrks");
+    std::cout << "-- Fitting tracks --" << std::endl;
+    FitTracks(vtxevents);
 
-        GetSyncedEventsFromClusTree(svxseg, svxcnt,
-                                    vtxevents, cntevents,
-                                    tgeo, nevents);
+    std::cout << "-- Filtering data --" << std::endl;
+    FilterData(vtxevents,
+               vertexprobmin,
+               vertexprobmax,
+               maxdca,
+               maxres_s,
+               maxres_z,
+               vtxtrks);
+  }
 
-        //need to fit segments to get phi, theta
-        std::cout << "-- Fitting tracks --" << std::endl;
-        FitTracks(vtxevents);
+  Printf("-- Writing output to %s --", outfilename);
+  outFile->cd();
+  outFile->Write(0, TObject::kOverwrite);
 
-        std::cout << "-- Filtering data --" << std::endl;
-        FilterData(vtxevents, cntevents,
-                   vtxtrks, cnttrks,
-                   vertexprobmin,
-                   vertexprobmax,
-                   maxdca,
-                   maxres_s,
-                   maxres_z);
+  // For now, just write the cnt ntuple to file without filtering.
+  if (opt.Contains("cnt"))
+  {
+    cnttrks->Write();
+  }
 
-    }
-    else
-    {
-
-        std::cout << "-- Reading events from ntuple --" << std::endl;
-        map <int, int> tmpmap;
-        GetEventsFromClusTree(svxseg, tgeo, vtxevents, tmpmap, nevents);
-
-        std::cout << "-- Fitting tracks --" << std::endl;
-        FitTracks(vtxevents);
-
-        std::cout << "-- Filtering data --" << std::endl;
-        FilterData(vtxevents,
-                   vertexprobmin,
-                   vertexprobmax,
-                   maxdca,
-                   maxres_s,
-                   maxres_z,
-                   vtxtrks);
-
-
-    }
-
-
-    Printf("-- Writing output to %s --", outfilename);
-    outFile->cd();
-    outFile->Write(0, TObject::kOverwrite);
-
-    // For now, just write the cnt ntuple to file without filtering.
-    if (opt.Contains("cnt"))
-    {
-        cnttrks->Write();
-    }
-
-    return;
+  return;
 }
