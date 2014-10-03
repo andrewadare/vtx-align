ef8168903a0b041bc8247cb1f9da8022bcde4511
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-10-02 21:25:29 -0400

    Used fixed beamcenter in fit to help reject outliers

--- FilterData.C
+++ FilterData.C
@@ -9,7 +9,10 @@
 // For record-keeping transparency, symlink the needed geometry file to
 // run-prod-iter.par so the base matches the ROOT file.
 // The alignment script VtxAlign.C requires a .par file with such a name.
-
+// opt may contain:
+//      "": vtx only. 
+//      "cnt": vtxtrks & cnttrks. 
+//      "fixed-bc" use beamcenter from rootfiles/bc-411768.root
 void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro0-no-vtx2cnt.root",
                 const char *outfilename = "rootfiles/411768-0-0.root",
                 const char *configfilename = "production/config/config-zf-411768-0-0.txt",
@@ -21,7 +24,7 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro0-no
                 int nhitsmin = 3,
                 int nevents = 10000, // -1 = everything
                 float frac4hit = -1, // -1 = no filter
-                TString opt = "cnt") // "": vtx only. "cnt": vtxtrks & cnttrks.
+                TString opt = "cnt, fixed-bc") // see above
 {
   std::cout << "-- Opening " << infilename << " --" << std::endl;
   TFile *inFile = new TFile(infilename, "read");
@@ -39,10 +42,29 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro0-no
   GetParamFromConfig(configfilename, bc, e2w, v2c, geo);
   SvxTGeo *tgeo = VTXModel(geo.c_str());
 
+  TGraphErrors *gbc;
+  if (opt.Contains("fixed-bc"))
+  {
+    //use fixed bc from designated file
+    TString bcFileIn   = "rootfiles/bc-411768.root";
+    TFile *bcf         = new TFile(bcFileIn.Data(), "read");
+    gbc  = (TGraphErrors *) bcf->Get("gbc");
+  }
+  else
+  {
+    //use bc from config file
+    gbc = new TGraphErrors();
+    gbc->SetTitle("Beamcenter from east arm (point 0) and west arm (point 1)");
+    gbc->SetPoint(0, bc[0] + e2w[0], bc[1] + e2w[1]);
+    gbc->SetPoint(1, bc[0], bc[1]);
+    gbc->SetPointError(0, 0.015, 0.010);
+    gbc->SetPointError(1, 0.015, 0.010);
+  }
+
+
   std::cout << "-- Creating output file " << outfilename << " --" << std::endl;
   TFile *outFile = new TFile(outfilename, "recreate");
-  // TNtuple *vtxhits = new TNtuple("vtxhits", "VTX hit variables", HITVARS);
-  // TNtuple *cnthits = new TNtuple("cnthits", "CNT hit variables", HITVARS);
+
 
   std::cout << "-- Creating vtxtrks Tree --" << std::endl;
   TTree *vtxtrks = CreateTree("vtxtrks");
@@ -66,7 +88,13 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro0-no
 
     //need to fit segments to get phi, theta
     std::cout << "-- Fitting tracks --" << std::endl;
-    FitTracks(vtxevents);
+    if (gbc)
+    {
+      Info("", "Using beamcenter in fit:");
+      Info("", " E: (%.3f, %.3f)", gbc->GetX()[0], gbc->GetY()[0]);
+      Info("", " W: (%.3f, %.3f)", gbc->GetX()[1], gbc->GetY()[1]);
+    }
+    FitTracks(vtxevents, gbc);
 
     std::cout << "-- Filtering data --" << std::endl;
     FilterData(vtxevents, cntevents,
@@ -86,7 +114,13 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-pro0-no
     GetEventsFromClusTree(svxseg, tgeo, vtxevents, tmpmap, nevents);
 
     std::cout << "-- Fitting tracks --" << std::endl;
-    FitTracks(vtxevents);
+    if (gbc)
+    {
+      Info("", "Using beamcenter from %s", bcf->GetName());
+      Info("", " E: (%.3f, %.3f)", gbc->GetX()[0], gbc->GetY()[0]);
+      Info("", " W: (%.3f, %.3f)", gbc->GetX()[1], gbc->GetY()[1]);
+    }
+    FitTracks(vtxevents, gbc);
 
     std::cout << "-- Filtering data --" << std::endl;
     FilterData(vtxevents,
