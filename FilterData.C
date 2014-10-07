72f0d8d10f591218c2e3900e5b2ead8483b4057f
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-10-07 11:51:33 -0600

    Use new FilterData() function, and add the ability to re-fit the filtered tracks before writing to file.

--- FilterData.C
+++ FilterData.C
@@ -12,10 +12,12 @@
 // opt may contain:
 //      "": vtx only.
 //      "cnt": vtxtrks & cnttrks.
-//      "fixed-bc" use beamcenter from rootfiles/bc-411768.root
+//      "no-refit": don't refit the tracks after filtering
+//      "fixed-bc": use beamcenter from rootfiles/bc-411768.root when refitting
+//                  tracks after filtering
 
-void FilterData(const char *infilename = "rootfiles/anavtxcluster_zf-411768-0-1_full.root",
-                const char *outfilename = "rootfiles/411768-0-1.root",
+void FilterData(const char *infilename = "rootfiles/anavtxcluster_411768-0-0_5_full.root",
+                const char *outfilename = "rootfiles/411768-0-0.root",
                 const char *configfilename = "production/config/config-zf-411768-0-0_5.txt",
                 double vertexprobmin = 0.02,
                 double vertexprobmax = 0.98,
@@ -25,17 +27,17 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_zf-411768-0-1_
                 int nhitsmin = 3,
                 int nevents = -1, // -1 = everything
                 float frac4hit = -1, // -1 = no filter
-                TString opt = "no-bc") // see above
+                TString opt = "fixed-bc") // see above
 {
   std::cout << "-- Opening " << infilename << " --" << std::endl;
   TFile *inFile = new TFile(infilename, "read");
 
-  std::cout << "-- Reading seg_clusntuple --" << std::endl;
+  std::cout << "\n-- Reading seg_clusntuple --" << std::endl;
   TNtuple *svxseg = (TNtuple *)inFile->Get("seg_clusntuple");
   TNtuple *svxcnt;
   assert(svxseg);
 
-  std::cout << "-- Initializing SvxTGeo --" << std::endl;
+  std::cout << "\n-- Initializing SvxTGeo --" << std::endl;
   float bc[2] = {0};
   float e2w[3] = {0};
   float v2c[3] = {0};
@@ -43,10 +45,61 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_zf-411768-0-1_
   GetParamFromConfig(configfilename, bc, e2w, v2c, parFileName);
   SvxTGeo *tgeo = VTXModel(parFileName.c_str());
 
-  TGraphErrors *gbc = NULL;
-  if (opt.Contains("no-bc"))
+  std::cout << "\n-- Creating output file " << outfilename << " --" << std::endl;
+  TFile *outFile = new TFile(outfilename, "recreate");
+
+  std::cout << "\n-- Creating vtxtrks Tree --" << std::endl;
+  TTree *vtxtrks = CreateTree("vtxtrks");
+  TTree *cnttrks;
+
+  geoEvents vtxevents;
+  geoEvents cntevents;
+
+  if (opt.Contains("cnt"))
   {
-    //don't use a beamcenter in the fit
+    std::cout << "\n-- Reading cnt_clusntuple --" << std::endl;
+    svxcnt = (TNtuple *) inFile->Get("cnt_clusntuple");
+    assert(svxcnt);
+
+    std::cout << "\n-- Creating cnttrks Tree --" << std::endl;
+    cnttrks = CreateTree("cnttrks");
+
+    GetSyncedEventsFromClusTree(svxseg, svxcnt,
+                                vtxevents, cntevents,
+                                tgeo, nevents);
+  }
+  else
+  {
+    std::cout << "\n-- Reading events from ntuple --" << std::endl;
+    map <int, int> tmpmap;
+    GetEventsFromClusTree(svxseg, tgeo, vtxevents, tmpmap, nevents);
+  }
+
+  //need to fit segments to get phi, theta
+  //NOTE: For the purposes of rejecting outlider, the
+  //      first fit should always find the primary vertex
+  //      and calculate dca with respect to that.
+  std::cout << "\n-- Fitting tracks --" << std::endl;
+  FitTracks(vtxevents, 0, "find_vertex, calc_dca");
+
+  std::cout << "\n-- Filtering data --" << std::endl;
+  geoEvents accvtxevents;
+  geoEvents acccntevents;
+  FilterData(vtxevents, cntevents,
+             accvtxevents, acccntevents,
+             vertexprobmin, vertexprobmax,
+             maxdca,
+             maxres_s, maxres_z,
+             10,
+             10,
+             nhitsmin,
+             frac4hit);
+
+  //refit tracks with fixed bc if desired
+  TGraphErrors *gbc = NULL;
+  if (opt.Contains("no-refit"))
+  {
+    //don't refit
   }
   else if (opt.Contains("fixed-bc"))
   {
@@ -66,62 +119,19 @@ void FilterData(const char *infilename = "rootfiles/anavtxcluster_zf-411768-0-1_
     gbc->SetPointError(1, 0.015, 0.010);
   }
 
-
-  std::cout << "-- Creating output file " << outfilename << " --" << std::endl;
-  TFile *outFile = new TFile(outfilename, "recreate");
-
-  std::cout << "-- Creating vtxtrks Tree --" << std::endl;
-  TTree *vtxtrks = CreateTree("vtxtrks");
-  TTree *cnttrks;
-
-  geoEvents vtxevents;
-  geoEvents cntevents;
-
-  if (opt.Contains("cnt"))
-  {
-    std::cout << "-- Reading cnt_clusntuple --" << std::endl;
-    svxcnt = (TNtuple *) inFile->Get("cnt_clusntuple");
-    assert(svxcnt);
-
-    std::cout << "-- Creating vtxtrks Tree --" << std::endl;
-    cnttrks = CreateTree("cnttrks");
-
-    GetSyncedEventsFromClusTree(svxseg, svxcnt,
-                                vtxevents, cntevents,
-                                tgeo, nevents);
-  }
-  else
-  {
-    std::cout << "-- Reading events from ntuple --" << std::endl;
-    map <int, int> tmpmap;
-    GetEventsFromClusTree(svxseg, tgeo, vtxevents, tmpmap, nevents);
-  }
-
-  //need to fit segments to get phi, theta
-  std::cout << "-- Fitting tracks --" << std::endl;
-
   if (gbc)
   {
-    Info("", "Using beamcenter in fit:");
+    Printf("\n-- Reffitting tracks with desired bc --");
     Info("", " E: (%.3f, %.3f)", gbc->GetX()[0], gbc->GetY()[0]);
     Info("", " W: (%.3f, %.3f)", gbc->GetX()[1], gbc->GetY()[1]);
+    FitTracks(accvtxevents, gbc, "find_vertex, calc_dca");
   }
-  FitTracks(vtxevents, gbc, "find_vertex, calc_dca");
 
-  std::cout << "-- Filtering data --" << std::endl;
-  FilterData(vtxevents, cntevents,
-             vtxtrks, cnttrks,
-             gbc,
-             vertexprobmin, vertexprobmax,
-             maxdca,
-             maxres_s, maxres_z,
-             10,
-             10,
-             nhitsmin,
-             frac4hit);
+  Printf("\n-- Writing output to %s --", outfilename);
+  FillTree(accvtxevents, vtxtrks);
+  if (cntevents.size() > 0)
+    FillTree(acccntevents, cnttrks);
 
-
-  Printf("-- Writing output to %s --", outfilename);
   outFile->cd();
   outFile->Write(0, TObject::kOverwrite);
 
