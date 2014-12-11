bb06fee4596aa5e53379407abe7ee211bfb7ce06
Andrew Adare <andrewadare@gmail.com>
2014-12-11 15:11:58 -0700

    Arm xyz translation. As run for 5-1.

--- VtxAlign.C
+++ VtxAlign.C
@@ -33,9 +33,9 @@ void CorrectFromFile(const char *filename,
                      geoEvents &cntevents);
 
 void VtxAlign(int run = 411768,    // Run number of PRDF segment(s)
-              int prod = 4,        // Production step. Starts at 0.
+              int prod = 5,        // Production step. Starts at 0.
               int subiter = 0,     // Geometry update step. Starts at 0.
-              TString alignMode = "ladder") // "arm","ladder","halflayer" (+"sim")
+              TString alignMode = "arm") // "arm","ladder","halflayer" (+"sim")
 {
   // No point in continuing if Millepede II is not installed...
   if (TString(gSystem->GetFromPipe("which pede")).IsNull())
@@ -59,11 +59,11 @@ void VtxAlign(int run = 411768,    // Run number of PRDF segment(s)
   // Assign free global parameter coords for dz residuals here
   // Parameter set may include "x", "y", "z", "r".
   // Also assign presigma list for these coordinates (trumps defaultPreSigma).
-  vecs zgpars {"z", "r"};
-  vecd zgpresigma {0, 0};
+  // vecs zgpars {"z", "r"};
+  // vecd zgpresigma {0, 0};
 
-  // vecs zgpars {"z"};
-  // vecd zgpresigma {0};
+  vecs zgpars {"z"};
+  vecd zgpresigma {0};
 
   // For Arm alignment with SvxCntTracks
   // vecs zgpars {"pitch", "yaw"};
@@ -80,17 +80,17 @@ void VtxAlign(int run = 411768,    // Run number of PRDF segment(s)
     // vecd p {0, 0, 0, 0, 0};
     // sgpars.resize(5);
 
-    vecs s {"pitch", "yaw", "roll"};
-    vecd p {0, 0, 0};
-    sgpars.resize(3);
+    // vecs s {"pitch", "yaw", "roll"};
+    // vecd p {0, 0, 0};
+    // sgpars.resize(3);
 
     // vecs s {"x", "y", "pitch", "roll"};
     // vecd p {0,0,0,0};
     // sgpars.resize(4);
 
-    // vecs s {"x", "y", "roll"};
-    // vecd p {0,0,0};
-    // sgpars.resize(3);
+    vecs s {"x", "y"};
+    vecd p {0,0};
+    sgpars.resize(2);
 
     sgpars = s;
     sgpresigma = p;
