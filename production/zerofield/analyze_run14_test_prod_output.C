1517741ab0579fa05d866328ba1e3f02955916c5
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-12-19 16:39:33 -0500

    Use the new AnaAlignmentProd module and clean up

--- production/zerofield/analyze_run14_test_prod_output.C
+++ production/zerofield/analyze_run14_test_prod_output.C
@@ -1,17 +1,9 @@
 analyze_run14_test_prod_output(int nevents = 0,
                                   const char *dstFile = "",
                                   const char *outfile = "test_testvtxproduction.root"
-                                  //const char *treefile = "test_svxcnttree.root",
-                                  //const char *histfile = "test_svxeventhist.root",
-                                  //const char *eventfile = "test_svxeventtree.root"
                                  )
 {
     gROOT->SetBatch(true);
-
-    //const char *infile1 = "/direct/phenix+prod05/phnxreco/run14_online/run14_online_ca/run_0000402000_0000403000/CNT/CNT_run14_online_ca-0000402782-0000.root";
-    //const char *infile2  = "/direct/phenix+prod05/phnxreco/run14_online/run14_online_ca/run_0000402000_0000403000/DST_SVX/DST_SVX_run14_online_ca-0000402782-0000.root";
-    //const char *infile3  = "/direct/phenix+prod05/phnxreco/run14_online/run14_online_ca/run_0000402000_0000403000/DST_EVE/DST_EVE_run14_online_ca-0000402782-0000.root";
-
 
     gSystem->Load("libfun4all.so");
     gSystem->Load("libfun4allfuncs.so");
@@ -21,12 +13,11 @@ analyze_run14_test_prod_output(int nevents = 0,
     gSystem->Load("libsimreco.so");
     gSystem->Load("librecal.so");
 
-    //gSystem->Load("libRun14EventTrigger.so");
-    //gSystem->Load("libRun11AuAuSvxCentralTrackTree.so");
-    //gSystem->Load("libRun11AuAuSvxEventHists.so");
-    //gSystem->Load("libRun11AuAuSvxEventTree.so");
-    gSystem->Load("libTestVTXProduction.so");
+
+    gSystem->Load("libAnaAlignmentProd.so");
     gSystem->Load("libRun11VTXana.so");
+
+    gSystem->ListLibraries();
 
     Fun4AllServer *se = Fun4AllServer::instance();
     se->Verbosity(0);
@@ -41,7 +32,7 @@ analyze_run14_test_prod_output(int nevents = 0,
     //trig->set_bbczcut(10);
     //se->registerSubsystem(trig);
 
-    TestVTXProduction *svxtest = new TestVTXProduction(outfile);
+    AnaAlignmentProd *svxtest = new AnaAlignmentProd(outfile);
     svxtest->SetZeroField(true);
     se->registerSubsystem(svxtest);
 
@@ -56,10 +47,6 @@ analyze_run14_test_prod_output(int nevents = 0,
     se->run(nevents);
 
     se->End();
-
-    //Fun4AllHistoManager *hm;
-    //hm = se->getHistoManager("Run11AuAuSvxEventHists");
-    //hm->dumpHistos(histfile);
 
     cout << "Analysis finished " << endl;
 
